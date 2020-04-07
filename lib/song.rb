8031e97752d987237245e5ba30994db620f92b43
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-07 23:54:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,4 +19,9 @@ class Song
     @artist = Artist.find_or_create_by_name(artist)
   end 
   
+  def self.new_by_filename(filename) 
+    song = Song.new(filename.split(" - ")[1])
+    song.artist = Artist.new(filename.split(" - ")[0])
+    song
+  end
 end 
