40f53b502cb5a398b668959764c9f293bffe462c
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-07 23:20:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,11 +4,15 @@ class Artist
   
   def initialize(name)
     @name = name 
-    @@all << self  
+    save   
   end 
   
   def self.all 
     @@all 
+  end 
+  
+  def save
+    @@all << self 
   end 
   
 end 
